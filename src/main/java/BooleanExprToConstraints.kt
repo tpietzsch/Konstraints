f80fa4be9b94c43eced943aeb109f0c359a1f804
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2018-01-09 18:11:38 +0100

    latex output

--- src/main/java/BooleanExprToConstraints.kt
+++ src/main/java/BooleanExprToConstraints.kt
@@ -50,7 +50,7 @@ expression examples:
 
 		val tex = line.hasOption("t")
 		val source = line.args.joinToString(" ")
-		val interactive = source.isBlank()
+		val interactive = true;//source.isBlank()
 
 		run(source, interactive, tex)
 	} catch (exp : ParseException) {
@@ -76,17 +76,19 @@ private fun run(source : String, tex : Boolean = false) {
 		val cnf = expr.toCNF()
 		val constraints = constraints(cnf)
 
+		println("Input: \n    " + expr.toString())
+		println("CNF: \n    " + cnf.prettyPrint())
+		println("Constraints")
+		constraints.forEach { println("    " + it.toText()) }
+		println()
+
 		if (tex) {
-			println("Input: \n    " + expr.toString())
-			println("CNF: \n    " + cnf.prettyPrint())
+			println("Input:\n\\[\n  ${expr.toLatex()}\n\\]")
+			println("CNF:\n\\[\n  ${cnf.toLatex()}\n\\]" )
 			println("Constraints:\n\\[\n  \\begin{array}{rrcl}")
 			constraints.forEach { println("    " + it.toLatex() + "\\\\") }
 			println("  \\end{array}\n\\]")
-		} else {
-			println("Input: \n    " + expr.toString())
-			println("CNF: \n    " + cnf.prettyPrint())
-			println("Constraints")
-			constraints.forEach { println("    " + it.toText()) }
+			println()
 		}
 	} catch (e : ParseError) {
 	}
@@ -149,3 +151,31 @@ private fun <T> ClauseConstraint<T>.toLatex() : String {
 
 	return squant + (if (allQuant.isEmpty()) "& " else " :& ") + "${slhs} &\\ge & ${srhs}";
 }
+
+// extension functions CANNOT be polymorphic (statically resolved)
+private fun <T> BooleanExpr<T>.toLatex() : String = when(this) {
+	is Atom -> this.toLatex()
+	is NotExpr -> this.toLatex()
+	is AndExpr -> this.toLatex()
+	is OrExpr -> this.toLatex()
+	is ImplExpr -> this.toLatex()
+	is EquExpr -> this.toLatex()
+	is GenDisj -> this.toLatex()
+	is GenConj -> this.toLatex()
+	else -> ""
+}
+private fun <T> Atom<T>.toLatex() = toString()
+private fun <T> NotExpr<T>.toLatex() = "\\neg ${a.toLatex()}"
+private fun <T> AndExpr<T>.toLatex() = "\\left( ${a.toLatex()} \\land ${b.toLatex()} \\right)"
+private fun <T> OrExpr<T>.toLatex() = "\\left( ${a.toLatex()} \\lor ${b.toLatex()} \\right)"
+private fun <T> ImplExpr<T>.toLatex() = "\\left( ${a.toLatex()} \\longrightarrow ${b.toLatex()} \\right)"
+private fun <T> EquExpr<T>.toLatex() = "\\left( ${a.toLatex()} \\longleftrightarrow ${b.toLatex()} \\right)"
+private fun <T> GenDisj<T>.toLatex() = "\\left( \\bigvee_{${a} \\in ${setOfA}} ${a} \\right)"
+private fun <T> GenConj<T>.toLatex() = "\\left( \\bigwedge_{${a} \\in ${setOfA}} ${a} \\right)"
+
+private fun <T> Conjunction<T>.toLatex() : String =
+		this.joinToString(", ", "\\left[", "\\right]" ) {
+			clause -> clause.joinToString(", ", "\\left[", "\\right]") {
+				expr -> expr.toLatex()
+			}
+		}
